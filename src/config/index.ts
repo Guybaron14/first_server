1ea85a9c2d455ec5a48050b90ca7f27b399a5b36
Guybaron14 <guybar135@gmail.com>
2022-03-19 19:54:25 +0200

    feat: server template init

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+import * as env from 'env-var';
+import './dotenv';
+
+const config = {
+    service: {
+        port: env.get('PORT').required().asPortNumber(),
+        useCors: env.get('USE_CORS').default('false').asBool(),
+    },
+    mongo: {
+        uri: env.get('MONGO_URI').required().asUrlString(),
+        featureCollectionName: env.get('MONGO_FEATURE_COLLECTION_NAME').required().asString(),
+    },
+};
+
+export default config;
